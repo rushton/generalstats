cc4121622eeb903f306e7cae88fff3fef6073577
rushton <rushton.nicholas@gmail.com>
2022-12-19 11:21:32 -0800

    Add group-by functionality

--- cmd/generalstats/main.go
+++ cmd/generalstats/main.go
@@ -16,8 +16,9 @@ import (
 var groupBy *int = flag.Int("group-by", 0, "Groups stats by the provided column index, starting at 1.")
 
 func main() {
+	flag.Parse()
 	in := bufio.NewReader(os.Stdin)
-	dataMap := make(map[int]stats.Float64Data)
+	dataMaps := map[string]map[int]stats.Float64Data{}
 	for {
 		line, err := in.ReadString('\n')
 		if err == io.EOF {
@@ -28,55 +29,69 @@ func main() {
 		fields := strings.Fields(strings.TrimSuffix(line, "\n"))
 
 		for i := 0; i < len(fields); i++ {
-			datum, err := strconv.ParseFloat(fields[i], 64)
-			if err != nil {
-				log.Fatalf("error parsing data: %v", err)
+			if i+1 != *groupBy {
+				datum, err := strconv.ParseFloat(fields[i], 64)
+				if err != nil {
+					log.Fatalf("error parsing data: %v", err)
+				}
+				group := ""
+				if *groupBy != 0 {
+					group = fields[*groupBy-1]
+				}
+
+				if _, ok := dataMaps[group]; !ok {
+					dataMaps[group] = make(map[int]stats.Float64Data)
+				}
+				dataMaps[group][i] = append(dataMaps[group][i], datum)
 			}
-			dataMap[i] = append(dataMap[i], datum)
 		}
 	}
 
-	for _, data := range dataMap {
-		count := len(data)
+	for group, dataMap := range dataMaps {
+		for _, data := range dataMap {
+			count := len(data)
 
-		sum, err := data.Sum()
-		if err != nil {
-			log.Fatalf("Error calculating sum: %v", err)
-		}
-		min, err := data.Min()
-		if err != nil {
-			log.Fatalf("Error calculating min: %v", err)
-		}
-		max, err := data.Max()
-		if err != nil {
-			log.Fatalf("Error calculating max: %v", err)
-		}
-		mean, err := data.Mean()
-		if err != nil {
-			log.Fatalf("Error calculating mean: %v", err)
-		}
-		median, err := data.Median()
-		if err != nil {
-			log.Fatalf("Error calculating median: %v", err)
-		}
-		tp75, err := data.Percentile(75.0)
-		if err != nil {
-			log.Fatalf("Error calculating tp75: %v", err)
-		}
-		tp95, err := data.Percentile(95)
-		if err != nil {
-			log.Fatalf("Error calculating tp95: %v", err)
-		}
-		tp99, err := data.Percentile(99)
-		if err != nil {
-			log.Fatalf("Error calculating tp99: %v", err)
-		}
-		tp999, err := data.Percentile(99.9)
-		if err != nil {
-			log.Fatalf("Error calculating tp999: %v", err)
-		}
+			sum, err := data.Sum()
+			if err != nil {
+				log.Fatalf("Error calculating sum: %v", err)
+			}
+			min, err := data.Min()
+			if err != nil {
+				log.Fatalf("Error calculating min: %v", err)
+			}
+			max, err := data.Max()
+			if err != nil {
+				log.Fatalf("Error calculating max: %v", err)
+			}
+			mean, err := data.Mean()
+			if err != nil {
+				log.Fatalf("Error calculating mean: %v", err)
+			}
+			median, err := data.Median()
+			if err != nil {
+				log.Fatalf("Error calculating median: %v", err)
+			}
+			tp75, err := data.Percentile(75.0)
+			if err != nil {
+				log.Fatalf("Error calculating tp75: %v", err)
+			}
+			tp95, err := data.Percentile(95)
+			if err != nil {
+				log.Fatalf("Error calculating tp95: %v", err)
+			}
+			tp99, err := data.Percentile(99)
+			if err != nil {
+				log.Fatalf("Error calculating tp99: %v", err)
+			}
+			tp999, err := data.Percentile(99.9)
+			if err != nil {
+				log.Fatalf("Error calculating tp999: %v", err)
+			}
 
-		fmt.Printf(`sum: %.2f
+			if group != "" {
+				fmt.Printf("====== %s ======\n", group)
+			}
+			fmt.Printf(`sum: %.2f
 count: %d
 min: %.2f
 max: %.2f
@@ -87,5 +102,6 @@ tp95: %.2f
 tp99: %.2f
 tp999: %.2f
 `, sum, count, min, max, mean, median, tp75, tp95, tp99, tp999)
+		}
 	}
 }
